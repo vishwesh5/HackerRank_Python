3d2460b66616b1ff6e85b8436e3c592c6cec5619
vishwesh5 <vishweshshrimali5@gmail.com>
2018-03-04 16:28:10 +0530

    Create mutations.py

---
+++ Strings/mutations.py
@@ -0,0 +1,5 @@
+# mutations.py
+# Link: https://www.hackerrank.com/challenges/python-mutations/problem
+
+def mutate_string(string, position, character):
+    return string[:position]+character+string[position+1:]
