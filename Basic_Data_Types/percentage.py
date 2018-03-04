12c9e3bdbb28e9f05f9543c8e63e647d60d261ea
vishwesh5 <vishweshshrimali5@gmail.com>
2018-03-04 16:14:06 +0530

    Create percentage.py

---
+++ Basic_Data_Types/percentage.py
@@ -0,0 +1,12 @@
+# percentage.py
+# Link: https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    print("%.2f" % float(sum(student_marks[query_name])/3.0))
