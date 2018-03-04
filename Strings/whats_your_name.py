de5944425ce6c57aa0bdbae26e1d1baf7a3ce2f7
vishwesh5 <vishweshshrimali5@gmail.com>
2018-03-04 16:27:33 +0530

    Create whats_your_name.py

---
+++ Strings/whats_your_name.py
@@ -0,0 +1,5 @@
+# whats_your_name.py
+# Link: https://www.hackerrank.com/challenges/whats-your-name/problem
+
+def print_full_name(a, b):
+    print("Hello %s %s! You just delved into python."%(a,b))
