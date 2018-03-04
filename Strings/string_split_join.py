e9b6b59cc2f0120c5b285ac0a58a2eccdb8cbf0b
vishwesh5 <vishweshshrimali5@gmail.com>
2018-03-04 16:27:01 +0530

    Create string_split_join.py

---
+++ Strings/string_split_join.py
@@ -0,0 +1,6 @@
+# string_split_join.py
+# Link: https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+
+def split_and_join(line):
+    # write your code here
+    return '-'.join(line.split(" "))
