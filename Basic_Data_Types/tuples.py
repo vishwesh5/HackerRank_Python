f7b20d9b24fa712906bfdb59b342df7064cf441c
vishwesh5 <vishweshshrimali5@gmail.com>
2018-03-04 16:07:33 +0530

    Create tuples.py

---
+++ Basic_Data_Types/tuples.py
@@ -0,0 +1,8 @@
+# tuples.py
+# Link: https://www.hackerrank.com/challenges/python-tuples/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    integer_list = tuple(integer_list)
+    print(hash(integer_list))
